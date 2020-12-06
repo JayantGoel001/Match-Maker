50924330bf7cefbaea0eaf0ebef8d71f85243734
username <54479676+CormacKrum@users.noreply.github.com>
2020-12-06 10:47:25 +0530

    Initial Commit

--- match-maker.py
+++ match-maker.py
@@ -10,5 +10,15 @@ first = True
 previousX = 0
 previousY = 0
 
+button_symbol = [["\u2702", "\u2705", "\u2708", "\u2709", "\u270A", "\u270B"],
+                 ["\u270C", "\u270F", "\u2712", "\u2714", "\u2716", "\u2728"],
+                 ["\u2702", "\u2705", "\u2708", "\u2709", "\u270A", "\u270B"],
+                 ["\u270C", "\u270F", "\u2712", "\u2714", "\u2716", "\u2728"]]
+
+rd.shuffle(button_symbol)
+for i in button_symbol:
+    rd.shuffle(i)
+
+print(button_symbol)
 
 win.mainloop()
