6bc914db0792875af06cf89ce737c5e16c4e2d29
username <54479676+CormacKrum@users.noreply.github.com>
2020-12-06 10:34:15 +0530

    Initial Commit

---
+++ match-maker.py
@@ -0,0 +1,14 @@
+from tkinter import *
+import random as rd
+import time
+
+win = Tk()
+win.resizable(width=False, height=False)
+win.title = "Match Maker"
+
+first = True
+previousX = 0
+previousY = 0
+
+
+win.mainloop()
