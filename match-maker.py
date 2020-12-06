d75633f6e7173b545079e203df19ebde50786e0d
username <54479676+CormacKrum@users.noreply.github.com>
2020-12-06 13:08:50 +0530

    Initial Commit

--- match-maker.py
+++ match-maker.py
@@ -1,8 +1,7 @@
-from tkinter import *
-import tkinter.font as font
 import random as rd
 import time
 from functools import partial
+from tkinter import *
 
 win = Tk()
 win.resizable(width=False, height=False)
@@ -13,9 +12,9 @@ previousX = 0
 previousY = 0
 
 colors = ["#800000", "#9A7434", "#000075", "#808000", "#911eb4", "#3cb44b",
-          "#4363d8", "#f58231", "#42d4f4", "#ffe119", "#fabed4", "#ffffff",
+          "#4363d8", "#f58231", "#42d4f4", "#ffe119", "#fabed4", "#000000",
           "#800000", "#9A7434", "#000075", "#808000", "#911eb4", "#3cb44b",
-          "#4363d8", "#f58231", "#42d4f4", "#ffe119", "#fabed4", "#ffffff"]
+          "#4363d8", "#f58231", "#42d4f4", "#ffe119", "#fabed4", "#000000"]
 
 rd.shuffle(colors)
 
@@ -27,17 +26,41 @@ for i in range(len(colors)):
 
 buttons = {}
 
+count = 0
+
 
 def showSymbol(i, j):
-    global first, previousX, previousY
+    global first, previousX, previousY, count
     buttons[i, j].configure(bg=colors_symbol[i][j])
     buttons[i, j].update_idletasks()
+
+    if first:
+        previousX = i
+        previousY = j
+        first = False
+    elif previousX != i or previousY != j:
+        if colors_symbol[previousX][previousY] != colors_symbol[i][j]:
+            time.sleep(0.5)
+            buttons[i, j].configure(bg="white")
+            buttons[previousX, previousY].configure(bg="white")
+        else:
+            buttons[previousX, previousY]['command'] = DISABLED
+            buttons[i, j]['command'] = DISABLED
+            count += 2
+            if count == 24:
+                win2 = Tk()
+                text = Text(win2)
+                text.insert(INSERT, "Congratulation!!!\nYou Won")
+                text.pack()
+                win2.geometry("150x50")
+                win2.mainloop()
+        first = True
 
 
 for i in range(4):
     for j in range(6):
         args = partial(showSymbol, i, j)
-        button = Button(command=args, width=12, height=8, bg="black")
+        button = Button(command=args, width=12, height=8, bg="white")
         buttons[i, j] = button
         button.grid(row=i, column=j)
 
